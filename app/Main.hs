47e058b72b9fa19b957b075c09cacde6f33de65f
Michael Dürig <michid@gmail.com>
2021-01-30 11:49:32 +0100

    Refactor argument parsing

--- app/Main.hs
+++ app/Main.hs
@@ -44,52 +44,71 @@ main = do
   opts <- execParser (parserWithDefaultDir cd pmdRules)
   runWithOpts opts
   where
-    parserWithDefaultDir cd ruleFile = info (helper <*> argsParser cd ruleFile) mempty
+    parserWithDefaultDir cd ruleFile = info (helper <*> optsParser cd ruleFile) mempty
 
 runWithOpts :: Opts -> IO ()
-runWithOpts (Opts path PMD before after pmdRules gitDir normChurn normComplexity) 
-  = TechDebt.pmdHotspots normChurn normComplexity  gitDir pmdRules before after path
-runWithOpts (Opts path LOC before after _ gitDir normChurn normComplexity) 
+runWithOpts (Opts path PMD before after pmdRules gitDir normChurn normComplexity)
+  = TechDebt.pmdHotspots normChurn normComplexity gitDir pmdRules before after path
+runWithOpts (Opts path LOC before after _ gitDir normChurn normComplexity)
   = TechDebt.locHotspots normChurn normComplexity gitDir before after path
 
-argsParser :: String -> String -> Parser Opts
-argsParser dir pmdRules = Opts
-     <$>  optional (strArgument
-          ( metavar "<path>"
-         <> help "path within the Git repository"))
-     <*>  ( flag PMD PMD (long "pmd"
-         <> help "use pmd complexity metric")
-        <|> flag PMD LOC (long "loc"
-         <> help "use loc complexity metric"))
-     <*> optional ( strOption
-          ( metavar "<date>"
-         <> long "before"
-         <> short 'b'
-         <> help "only include commits before the specified date" ))
-     <*> optional ( strOption
-          ( metavar "<date>"
-         <> long "after"
-         <> short 'a'
-         <> help "only include commits after the specified date" ))
-     <*> strOption
-          ( metavar "<path>"
-         <> long "rule"
-         <> short 'r'
-         <> value pmdRules
-         <> internal
-         <> help "custom PMD rule.xml")
-     <*> strOption
-          ( metavar "<path>"
-         <> long "git-dir"
-         <> short 'g'
-         <> value dir
-         <> help "path to the Git repository")
-     <*> optional ( option auto 
-          ( metavar "<churn norm>"
-         <> long "churn-norm"
-         <> help "constant for normalizing the churn value"))
-     <*> optional ( option auto
-          ( metavar "<complexity norm>"
-         <> long "complexity-norm"
-         <> help "constant for normalizing the complexity value"))
-    
+optsParser :: String -> String -> Parser Opts
+optsParser dir pmdRules = Opts
+     <$> optional path
+     <*> ( pmd <|> loc )
+     <*> optional before
+     <*> optional after
+     <*> rule
+     <*> gitDir
+     <*> optional churnNorm
+     <*> optional complexityNorm
+  where
+    path = strArgument
+        (  metavar "<path>"
+        <> help "path within the Git repository"
+        )
+    pmd = flag PMD PMD
+        (  long "pmd"
+        <> help "use pmd complexity metric"
+        )
+    loc = flag PMD LOC
+        (  long "loc"
+        <> help "use loc complexity metric"
+        )
+    before = strOption
+        (  metavar "<date>"
+        <> long "before"
+        <> short 'b'
+        <> help "only include commits before the specified date"
+        )
+    after = strOption
+        (  metavar "<date>"
+        <> long "after"
+        <> short 'a'
+        <> help "only include commits after the specified date"
+        )
+    rule = strOption
+        (  metavar "<path>"
+        <> long "rule"
+        <> short 'r'
+        <> value pmdRules
+        <> internal
+        <> help "custom PMD rule.xml"
+        )
+    gitDir = strOption
+        (  metavar "<path>"
+        <> long "git-dir"
+        <> short 'g'
+        <> value dir
+        <> help "path to the Git repository"
+        )
+    churnNorm = option auto
+        (  metavar "<churn norm>"
+        <> long "churn-norm"
+        <> help "constant for normalizing the churn value"
+        )
+    complexityNorm = option auto
+        (  metavar "<complexity norm>"
+        <> long "complexity-norm"
+        <> help "constant for normalizing the complexity value"
+        )
