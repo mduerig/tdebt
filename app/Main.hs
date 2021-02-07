4a82f8a697ef603e5097c925c799b3722363ed6c
Michael Dürig <michid@gmail.com>
2021-02-07 14:09:07 +0100

    Add option for formating output as CSV

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,5 @@
+{-#LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import qualified TechDebt
@@ -23,6 +25,8 @@ import Options.Applicative
 import Data.Monoid ((<>))
 import System.Directory (getCurrentDirectory)
 import Paths_tdebt(getDataFileName)
+import qualified Data.Csv as CSV
+import qualified Data.ByteString.Lazy.Char8 as BSL
 
 data Opts = Opts
   { path ::  Maybe String
@@ -32,9 +36,13 @@ data Opts = Opts
   , gitDir :: String
   , after :: Maybe String
   , sum :: Maybe Int
+  , outputFormat :: OutputFormat
   } deriving Show
 
 data Complexity = PMD | LOC
+  deriving Show
+
+data OutputFormat = RAW | CSV
   deriving Show
 
 main :: IO ()
@@ -46,14 +54,32 @@ main = do
   where
     parserWithDefaultDir cd ruleFile = info (helper <*> optsParser cd ruleFile) mempty
 
+csvEncode :: TechDebt.TDebt -> BSL.ByteString
+csvEncode tDebt =
+  let
+    tabulate (file, TechDebt.Metric churn complexity debt) = (file, churn, complexity, debt)
+  in
+    CSV.encode $ tabulate <$> tDebt
+
+raw :: TechDebt.TDebt -> IO ()
+raw = mapM_ print
+
+csv :: TechDebt.TDebt -> IO ()
+csv tDebt = BSL.putStr $ csvEncode tDebt
+
+toFormat :: OutputFormat -> TechDebt.TDebt -> IO ()
+toFormat RAW = raw
+toFormat CSV = csv
+
 runWithOpts :: Opts -> IO ()
-runWithOpts (Opts path complexity perFile pmdRules gitDir after count) = do
+runWithOpts (Opts path complexity perFile pmdRules gitDir after count format) = do
   tDebt <- case complexity of
     PMD -> TechDebt.pmdHotspots gitDir after pmdRules path
     LOC -> TechDebt.locHotspots gitDir after path
   case tDebt of
     Left msg -> putStrLn msg
-    Right debt -> mapM_ print
+    Right debt ->
+        toFormat format
       $ TechDebt.maybeSum count
       $ TechDebt.maybeNorm perFile
         debt
@@ -67,6 +93,7 @@ optsParser dir pmdRules = Opts
      <*> gitDir
      <*> optional after
      <*> optional summary
+     <*> (raw <|> csv)
   where
     path = strArgument
         (  metavar "<path>"
@@ -109,5 +136,13 @@ optsParser dir pmdRules = Opts
         <> long "sum"
         <> short 's'
         <> help "output the sum of the last <count> metrics"
+        )
+    raw = flag RAW RAW
+        (  long "raw"
+        <> help "raw output format"
+        )
+    csv = flag RAW CSV
+        (  long "csv"
+        <> help "CSV output format"
         )
 
